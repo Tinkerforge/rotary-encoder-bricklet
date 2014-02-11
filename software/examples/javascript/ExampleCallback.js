43d17af03f14c8768c6834647b236227cab92ef0
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-11 17:10:51 +0100

    javascript: add examples

---
+++ software/examples/javascript/ExampleCallback.js
@@ -0,0 +1,42 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickletRotaryEncoder = require('Tinkerforge/BrickletRotaryEncoder');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'utz';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var encoder = new BrickletRotaryEncoder(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);        
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Set Period for count callback to 0.05s (50ms)
+        // Note: The count callback is only called every 50ms if the 
+        // count has changed since the last call!
+        encoder.setCountCallbackPeriod(50);   
+    }
+);
+
+//Register count callback
+encoder.on(BrickletRotaryEncoder.CALLBACK_COUNT,
+    //Callback function for count callback
+    function(count) {
+        console.log('Count: '+count);
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
+
---
+++ software/examples/javascript/ExampleSimple.js
@@ -0,0 +1,39 @@
+var IPConnection = require('Tinkerforge/IPConnection');
+var BrickletRotaryEncoder = require('Tinkerforge/BrickletRotaryEncoder');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'utz';// Change to your UID
+
+var ipcon = new IPConnection();// Create IP connection
+var encoder = new BrickletRotaryEncoder(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);        
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Get current count of encoder without reset 
+        encoder.getCount(false,
+            function(count) {
+                console.log('Count: '+count);
+            },
+            function(error) {
+                console.log('Error: '+error);
+            }
+        );
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
+
