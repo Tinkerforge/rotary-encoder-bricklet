f0333e1ed506a83ac5e47f7acc740189357233cf
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:19:05 +0200

    java: Unify constants style

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -2,8 +2,8 @@ import com.tinkerforge.BrickletRotaryEncoder;
 import com.tinkerforge.IPConnection;
 
 public class ExampleCallback {
-	private static final String host = "localhost";
-	private static final int port = 4223;
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
 	private static final String UID = "XYZ"; // Change to your UID
 	
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
@@ -12,7 +12,7 @@ public class ExampleCallback {
 		IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletRotaryEncoder re = new BrickletRotaryEncoder(UID, ipcon); // Create device object
 
-		ipcon.connect(host, port); // Connect to brickd
+		ipcon.connect(HOST, PORT); // Connect to brickd
 		// Don't use device before ipcon is connected
 
 		// Set Period for count callback to 0.05s (50ms)
