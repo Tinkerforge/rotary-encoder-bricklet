c1d60d0c50bc53f48913e3814086f418aaa1fb44
Matthias Bolte <matthias@tinkerforge.com>
2015-02-13 17:03:52 +0100

    vbnet: Use ReadLine instead of ReadKey
    Because ReadKey doesn't work with a pipe as stdin on the RED Brick.

--- software/examples/vbnet/ExampleCallback.vb
+++ software/examples/vbnet/ExampleCallback.vb
@@ -26,7 +26,7 @@ Module ExampleCallback
         AddHandler encoder.Count, AddressOf CountCB
 
         System.Console.WriteLine("Press key to exit")
-        System.Console.ReadKey()
+        System.Console.ReadLine()
         ipcon.Disconnect()
     End Sub
 End Module
--- software/examples/vbnet/ExampleSimple.vb
+++ software/examples/vbnet/ExampleSimple.vb
@@ -18,7 +18,7 @@ Module ExampleSimple
         System.Console.WriteLine("Count: " + count.ToString())
 
         System.Console.WriteLine("Press key to exit")
-        System.Console.ReadKey()
+        System.Console.ReadLine()
         ipcon.Disconnect()
     End Sub
 End Module
