67c11894590c09f9937bddecffa0a7471b98d1ca
Matthias Bolte <matthias@tinkerforge.com>
2015-07-28 11:04:24 +0200

    Whitespace

--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -5,7 +5,7 @@ public class ExampleSimple {
 	private static final String HOST = "localhost";
 	private static final int PORT = 4223;
 	private static final String UID = "XYZ"; // Change to your UID
-	
+
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
 	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
@@ -17,7 +17,6 @@ public class ExampleSimple {
 
 		// Get current count without reset
 		int count = re.getCount(false); // Can throw com.tinkerforge.TimeoutException
-
 		System.out.println("Count: " + count);
 
 		System.out.println("Press key to exit"); System.in.read();
