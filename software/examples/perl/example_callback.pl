d5858297d6d7d6906cb416dd356c148d43ff60ec
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:12:10 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,35 +1,34 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletRotaryEncoder;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '7xwQ9g'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $encoder = Tinkerforge::BrickletRotaryEncoder->new(&UID, $ipcon); # Create device object
+my $re = Tinkerforge::BrickletRotaryEncoder->new(&UID, $ipcon); # Create device object
 
-# Callback function for count callback
+# Callback subroutine for count callback
 sub cb_count
 {
     my ($count) = @_;
 
-    print "Count: $count\n";
+    print "Count: " . $count . "\n";
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Set Period for count callback to 0.05s (50ms)
-# Note: The count callback is only called every 50ms if the 
-#       count has changed since the last call!
-$encoder->set_count_callback_period(50);
+# Set period for count callback to 0.05s (50ms)
+# Note: The count callback is only called every 0.05 seconds
+#       if the count has changed since the last call!
+$re->set_count_callback_period(50);
 
-# Register count callback to function cb_count
-$encoder->register_callback($encoder->CALLBACK_COUNT, 'cb_count');
+# Register count callback to subroutine cb_count
+$re->register_callback($re->CALLBACK_COUNT, 'cb_count');
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,23 +1,22 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletRotaryEncoder;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => '7xwQ9g'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $encoder = Tinkerforge::BrickletRotaryEncoder->new(&UID, $ipcon); # Create device object
+my $re = Tinkerforge::BrickletRotaryEncoder->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
-# Get current count of encoder without reset 
-my $count = $encoder->get_count(0);
-print "Count: $count";
+# Get current count without reset
+my $count = $re->get_count(0);
+print "Count: " . $count . "\n";
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
