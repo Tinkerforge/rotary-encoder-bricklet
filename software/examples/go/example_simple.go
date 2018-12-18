1dba67d0e48612dba5a4f6d8b9e07606502fa3f4
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:00:35 +0100

    go: Change import path.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/rotary_encoder_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/rotary_encoder_bricklet"
 )
 
 const ADDR string = "localhost:4223"
