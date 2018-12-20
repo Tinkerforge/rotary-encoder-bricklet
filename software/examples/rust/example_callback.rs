c85577797ff9f3149c7d06f78506f33588f745cd
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:58:58 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  rotary_encoder_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, rotary_encoder_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let re = RotaryEncoderBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let count_receiver = re.get_count_callback_receiver();
+    let count_receiver = re.get_count_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `re` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for count in count_receiver {           
-                		println!("Count: {}", count);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `re` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for count in count_receiver {
+            println!("Count: {}", count);
+        }
+    });
 
-		// Set period for count receiver to 0.05s (50ms).
-		// Note: The count callback is only called every 0.05 seconds
-		//       if the count has changed since the last call!
-		re.set_count_callback_period(50);
+    // Set period for count receiver to 0.05s (50ms).
+    // Note: The count callback is only called every 0.05 seconds
+    //       if the count has changed since the last call!
+    re.set_count_callback_period(50);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
