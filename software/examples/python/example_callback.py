86c19f8c1246a42f6c2d51dab603fc5badd3af41
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:22:58 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_callback.py
+++ software/examples/python/example_callback.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     #       if the count has changed since the last call!
     re.set_count_callback_period(50)
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
